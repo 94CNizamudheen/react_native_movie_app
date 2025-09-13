18f73db8b08e705c11e59013a4bf80ad25e1717c
Nizamudheen <nisunisam2@gmail.com>
2025-09-13 19:32:00 +0530

    initial tailwind setup

---
+++ react_native_mobile_app/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import './global.css'
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-5xl text-blue-700">hello world </Text>
+    </View>
+  );
+}
