4d80b45eb77a03cd61082a36eced66a1d376367d
Nizamudheen <nisunisam2@gmail.com>
2025-09-14 16:46:41 +0530

    started routing and styling

--- react_native_mobile_app/app/_layout.tsx
+++ react_native_mobile_app/app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-// import './global.css'
 export default function RootLayout() {
+  return (
 
-  return(
-    <SafeAreaProvider>
-      <Stack />;
-    </SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+
   ) 
 }
--- react_native_mobile_app/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-import './global.css'
-export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center bg-black">
-      <Text className="text-5xl text-blue-700">hello world </Text>
-    </View>
-  );
-}
